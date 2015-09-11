98c93f07a4c2dfd3bd52fbdc516445d48995d800
Thomas Lamiraud <Thomas.lamiraud@gmail.com>
2015-09-11 19:57:06 +0200

    add new back end code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,54 @@
 class SearchController < ApplicationController
   respond_to :json
 
+  # Most famous ports
+  H_ports = [{"port" => "21", "service" => "FTP"},
+                  {"port" => "22", "service" => "SSH"},
+                  {"port" => "23", "service" => "Telnet"},
+                  {"port" => "80", "service" => "HTTP"},
+                  {"port" => "88", "service" => "kerberos"},
+                  {"port" => "139", "service" =>	"NETBIOS Session Service"},
+                  {"port" => "443", "service" => "HTTPS"},
+                  {"port" => "445", "service" => "Smb"},
+                  {"port" => "1194", "service" => "OpenVPN"},
+                  {"port" => "1243", "service" => "serialgateway"},
+                  {"port" => "3306",	"service" => "Mysql Server"},
+                  {"port" => "5002", "service" => "radio free ethernet"},
+                  {"port" => "5432", "service" => "PostgreSQL"},
+                  {"port" => "5433", "service" => "pyrrho"},
+                  {"port" => "5900", "service" => "VNC Server"},
+                  {"port" => "9091", "service" => "Transmission"}]
+
   def index
     if params[:ip_address]
       re = "^(192\.168\.([0,1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([0,1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$"
 
       if params[:ip_address].match re
-        puts "Correct IP"
-        adress = IPAddr.new params[:ip_address]
+        tab_port = []
+        # puts "Correct IP"
 
-        if ping(adress.to_s)
-          puts "Scanning ..."
-          result = `nc -z #{adress} 1-1023`
+        # OLD CODE
+          # adress = IPAddr.new params[:ip_address]
+          adress = params[:ip_address]
+        if ping(adress)
+          # puts "Scanning ..."
+          # result = `nc -z #{adress} 1-1023`
 
-          puts "Result : #{result}"
+          # puts "Result : #{result}"
 
-          ports = result.split(/\n/)
+          for i in (1..1023) do
+            open_port(adress, i, tab_port)
+          end
 
-          json_port = ports.map do |p|
+          # OLD CODE
+            # ports = result.split(/\n/)
 
-            { :port => p.scan(/\w{4} \d{2,}/), :service => p.scan(/\/[a-z]+/).first.tr('/', ''), :status => "Open"}
+          json_port = tab_port.map do |p|
+
+            { :port => p.first["port"], :service => p.first["service"], :status => "Open"}
+
+            # OLD CODE
+              # { :port => p.scan(/\w{4} \d{2,}/), :service => p.scan(/\/[a-z]+/).first.tr('/', ''), :status => "Open"}
           end
 
         else
@@ -36,6 +65,7 @@ class SearchController < ApplicationController
     end
   end
 
+  # Use to ping IP to see if its available on the network
   def ping(host)
     begin
       Timeout.timeout(5) do
@@ -49,4 +79,18 @@ class SearchController < ApplicationController
       return false
     end
   end
+
+  # Method to find open port on a specific IP
+  def open_port(host, port, tab_port)
+    sock = Socket.new(:INET, :STREAM)
+    raw = Socket.sockaddr_in(port, host)
+    # puts "#{port} open." if sock.connect(raw)
+    if sock.connect(raw)
+      tab_port.push(H_ports.select{|port_number, service| port_number["port"] == port.to_s })
+    end
+
+  rescue (Errno::ECONNREFUSED)
+    rescue(Errno::ETIMEDOUT)
+  end
+
 end
